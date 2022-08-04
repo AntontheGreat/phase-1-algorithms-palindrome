4cad69dcf4f6e76d7877d670a9a854e39959d98c
Antonio <antonio18casta@aol.com>
2022-08-04 16:07:08 -0400

    commit tree 147df3c7e1a56790c286702221de00904baad7b3

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const reversed = word.split("").reverse().join("");
+  if (reversed === word) return true;
+
+    return false;
+};
 
 /* 
   Add your pseudocode here
+  checks if a string is a palindrome by taking a string and reversing it to see if its identical to the string prior to reversal.
 */
 
 /*
   Add written explanation of your solution here
+  My variable "reversed" splits the letters of the word into an array and then reverses the array and rejoins the letters.
+  Then the isPalindrome function checks if reversed is the same as word, if it yes the function returns true. If reversed is not the same as word, the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
